04f6f542af7ad413db30559e9622e7c27bd02380
Karani Karaman <karamankarani42@gmail.com>
2022-06-13 22:59:16 +0300

    docker hub push bat file created

---
+++ multi-layer/docker_hub.bat
@@ -0,0 +1,4 @@
+docker build --tag 2018141025_m:latest   .
+docker tag 2018141025_m:latest karanikaraman/2018141025_m:latest
+docker push karanikaraman/2018141025_m:latest
+pause
---
+++ single-layer/docker_hub.bat
@@ -0,0 +1,4 @@
+docker build --tag 2018141025_o:latest   .
+docker tag 2018141025_o:latest karanikaraman/2018141025_o:latest
+docker push karanikaraman/2018141025_o:latest
+pause
